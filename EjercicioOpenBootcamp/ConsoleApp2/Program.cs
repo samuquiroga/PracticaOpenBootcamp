7de0c2036f7d2bce77cf369258d0b184d461f92c
samuquiroga <113557597+samuquiroga@users.noreply.github.com>
2022-09-14 19:25:46 -0300

    CommitEjercicio

---
+++ EjercicioOpenBootcamp/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogicaEjercicio;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+            //EJERCICIO OPEN BOOTCAMP 
+            //Primera Parte
+        {
+            Console.WriteLine("Ingresa un numero");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa un numero");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa un numero");
+            int f = int.Parse(Console.ReadLine());
+
+            void sumaNumeros(int num1, int num2, int num3)
+            {
+                int resultado;
+                resultado = num1 + num2 + num3;
+                Console.WriteLine("El resultado de la suma es {0}", resultado);
+            }
+            sumaNumeros(x, y, f);
+            
+            //Segunda Parte
+            Coche coche = new Coche();
+            Console.WriteLine("Ingresa un numero de puertas que quieras agregar");
+            int g = int.Parse(Console.ReadLine());
+            coche.incrementoPuertas(g);
+            Console.WriteLine("El coche tiene {0} puertas", coche.numPuertas);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ EjercicioOpenBootcamp/LogicaEjercicio/Coche.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaEjercicio
+{
+    public class Coche
+    {
+        public int numPuertas { get; set; }
+
+        public void incrementoPuertas(int puertas)
+        {
+            if (puertas > 0 & puertas <= 5)
+            {
+                numPuertas = numPuertas + puertas;
+            }
+           else
+            {
+                Console.WriteLine("ingrese un valor valido");
+            }
+        
+        }
+
+
+    }
+}
